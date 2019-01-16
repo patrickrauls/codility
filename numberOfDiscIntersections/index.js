15364cec23a9796ec52ce2e5ace2492b8b440b96
Patrick Rauls <prauls@i3logix.com>
2019-01-16 03:41:00 -0700

    Liner complexity for Number of Disc Intersections

--- numberOfDiscIntersections/index.js
+++ numberOfDiscIntersections/index.js
@@ -1,11 +1,19 @@
 const solution = A => {
-    let answer;
+    let answer = 0;
+    let outstanding = 0;
     let starts = new Array(A.length);
     starts.fill(0);
+    let ends = starts.slice();
     A.forEach((disc, index) => {
         starts[Math.max(index - disc, 0)]++;
+        ends[Math.min(index + disc, A.length - 1)]++;
     });
-    answer = starts.length * (starts.length + 1) / 2 - starts.length - starts[0];
+    starts.forEach((start, index) => {
+        if (start) {
+            answer += outstanding * start + (start - 1) * start / 2;
+        }
+        outstanding += start - ends[index];
+    });
     return answer > 10000000 ? -1 : answer;
 };
 
--- test/numberOfDiscIntersections.test.js
+++ test/numberOfDiscIntersections.test.js
@@ -9,12 +9,13 @@ describe('Number Of Disc Intersections', () => {
         it('returns 11 given [1, 5, 2, 1, 4, 0]', () => {
             expect(numberOfDiscIntersections([1, 5, 2, 1, 4, 0])).to.equal(11);
         });
+    });
     describe('edge cases', () => {
         it('returns 4 given [1, 3, 0, 1]', () => {
             expect(numberOfDiscIntersections([1, 3, 0, 1])).to.equal(4);
         });
-        it('returns 2 given [1, 0, 1]', () => {
-            expect(numberOfDiscIntersections([1, 0, 1])).to.equal(2);
+        it('returns 3 given [1, 0, 1]', () => {
+            expect(numberOfDiscIntersections([1, 0, 1])).to.equal(3);
         });
         it('returns 13 given [6, 1, 0, 2, 0, 5]', () => {
             expect(numberOfDiscIntersections([6, 1, 0, 2, 0, 5])).to.equal(13);
@@ -22,6 +23,11 @@ describe('Number Of Disc Intersections', () => {
         it('returns 0, given [0, 0]', () => {
             expect(numberOfDiscIntersections([0, 0])).to.equal(0);
         });
-    });
+        it('returns 0, given [0, 0, 0, 0, 0]', () => {
+            expect(numberOfDiscIntersections([0, 0, 0, 0, 0])).to.equal(0);
+        });
+        it('returns 0, given []', () => {
+            expect(numberOfDiscIntersections([])).to.equal(0);
+        });
     });
 });
